ef18cc20ab193fcb7b2e8dcd0422dc2520254967
Syedtahafaiz <tahafaiz247@gmail.com>
2025-06-27 15:05:37 +0500

    dfshdfh

--- quiz-hub/src/pages/Auth/Login.jsx
+++ quiz-hub/src/pages/Auth/Login.jsx
@@ -3,7 +3,13 @@ import './Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+      <hr/>
+      <div className='inputpro'>
+         <input type='text' name='username' placeholder='Username'></input>
+         <input type='password' name='password' placeholder='password'></input>
+      </div>
+    </div>
   )
 }
 
