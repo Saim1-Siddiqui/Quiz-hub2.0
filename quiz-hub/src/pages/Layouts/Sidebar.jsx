03c5542cab28d6739583d46294430eaec6806b41
M.Saim siddiqui <saimsaddiqui3@gmail.com>
2025-06-30 04:08:20 -0700

    sasa

--- quiz-hub/src/pages/Layouts/Sidebar.jsx
+++ quiz-hub/src/pages/Layouts/Sidebar.jsx
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom';
 import { GiAtom } from 'react-icons/gi';
 import { TbMath } from 'react-icons/tb';
 import { MdComputer, MdDashboard } from 'react-icons/md';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -13,6 +15,9 @@ function Sidebar() {
     <aside className='main-sidebar'>
       {/* <h1 className="logo">Side-bar</h1> */}
       <nav className="nav">
+         <NavLink to="/Dashboard" className="Quiz-hub">
+         <FontAwesomeIcon icon={faHouse} className="icon" />Dashboard
+        </NavLink>
         <NavLink to ="/Science" className="Quiz-hub"> 
         <GiAtom className="icon"/> Science
         </NavLink>
@@ -22,9 +27,7 @@ function Sidebar() {
         <NavLink to="/Computer" className="Quiz-hub">
          <MdComputer className='icon' />Categories
         </NavLink>
-        <NavLink to="/Dashboard" className="Quiz-hub">
-         <MdDashboard className='icon' />Dashboard
-        </NavLink>
+       
       </nav>
     </aside>
   )
