6a8743819d7ad4e28c1995c1db1223d94bc5c7d6
M.Saim siddiqui <saimsaddiqui3@gmail.com>
2025-07-01 03:30:39 -0700

    saim

--- quiz-hub/src/pages/Dashboard/Dashboard.jsx
+++ quiz-hub/src/pages/Dashboard/Dashboard.jsx
@@ -1,53 +1,45 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Dashboard.css';
 import { NavLink } from 'react-router-dom';
-
-
-const quizzes = [
-  {
-    title: "Fun Quiz",
-    image: "../../../public/Images/fun.jpg"
-  },
-  {
-    title: "Science Quiz",
-    image: "../../../public/Images/science.jpg"
-  },
-  {
-    title: "Math Quiz",
-    image: "../../../public/Images/math.jpg"
-  },
-  {
-    title: "Computer Quiz",
-    image: "../../../public/Images/computer.jpg"
-  }
-];
+import { useQuiz } from '../../store/Context-api';
 
 function Dashboard() {
-  return (
-  <div className="dashboard-container">
-      <h1>Your Quizzes</h1>
+  const { data } = useQuiz();
 
+  return (
+    <div className="dashboard-container">
+      <h1>Your Quizzes</h1>
       <section className="featured-quizzes">
         <h2>Featured Quizzes</h2>
         <div className="quiz-grid">
-          {quizzes.map((quiz, index) => (
-            <div className="quiz-card" key={index}>
-              <img src={quiz.image} alt={quiz.title} />
-              <p>{quiz.title}</p>
-            </div>
+          {data.map((quiz, index) => (
+            <NavLink
+              to={`/quizintro/${quiz.category}`}
+              key={index}
+              className="quiz-card"
+            >
+              <img src={`/Images/${quiz.category}.jpg`} alt={quiz.title} />
+              <div className="quiz-info">
+                <p className="quiz-title">{quiz.title}</p>
+                
+              </div>
+            </NavLink>
           ))}
         </div>
       </section>
-
+   
       <section className="create-quiz">
         <h2>Create Your Own Quiz</h2>
         <div className="create-box">
           <h3>Create a new quiz</h3>
           <p>Add your own questions and challenge your friends</p>
-          <NavLink to="/createquiz" className="dash-link">Create-Quiz</NavLink>
+          <NavLink to="/createquiz" className="dash-link">
+            Create-Quiz
+          </NavLink>
         </div>
       </section>
     </div>
   );
-};
+}
+
 export default Dashboard;
