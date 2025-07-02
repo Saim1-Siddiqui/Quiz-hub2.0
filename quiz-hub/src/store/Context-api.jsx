184577e8152e5d2cf13646b10d503849539689d6
Zain Ishfaq <zainishfaq707@gmail.com>
2025-07-02 15:38:03 +0500

    xrtf6oivyt

--- quiz-hub/src/store/Context-api.jsx
+++ quiz-hub/src/store/Context-api.jsx
@@ -1,24 +1,22 @@
-import { createContext,useContext } from "react";
- export const QuizContext= createContext()
- import {data} from '../assets/data'
+// import { createContext,useContext } from "react";
+//  export const QuizContext= createContext()
+//  import {data} from '../assets/data'
   
- export const QuizProvider=({children})=>{
-    const myname="Mohsan";
-    const myage=21;
+//  export const QuizProvider=({children})=>{
+//     const myname="Mohsan";
+//     const myage=21;
      
-    return <QuizContext.Provider value={{myname,myage,data}}>
-        {children}
-    </QuizContext.Provider>
-  }
+//     return <QuizContext.Provider value={{myname,myage,data}}>
+//         {children}
+//     </QuizContext.Provider>
+//   }
 
-  export const useQuiz=()=>{
-    const context=useContext(QuizContext)
-    try {
-        return context
-    } catch (error) {
-        console.log(error,"error from context")
+//   export const useQuiz=()=>{
+//     const context=useContext(QuizContext)
+//     try {
+//         return context
+//     } catch (error) {
+//         console.log(error,"error from context")
         
-    }
-  }
-
-  
+//     }
+//   }
